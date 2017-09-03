d0a1cdf06001f83b368cf85b8b607f8fa48dfcca
Nils <magiccraft.nitrado@gmail.com>
2017-09-03 18:29:21 +0200

    :tada: Initial Commit

---
+++ DSBLibrary/DSBLibrary.h
@@ -0,0 +1,19 @@
+//
+//  DSBLibrary.h
+//  DSBLibrary
+//
+//  Created by Nils Bergmann on 03.09.17.
+//  Copyright © 2017 Noim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DSBLibrary.
+FOUNDATION_EXPORT double DSBLibraryVersionNumber;
+
+//! Project version string for DSBLibrary.
+FOUNDATION_EXPORT const unsigned char DSBLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DSBLibrary/PublicHeader.h>
+
+
